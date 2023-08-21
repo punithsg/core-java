e3758b42973d397ac660b144672a986f732d04e9
Prashantb07 <prashantb.xworkz@gmail.com>
2023-08-21 11:21:20 +0530

    commiting if loop related example

---
+++ ZomatoQ.java
@@ -0,0 +1,547 @@
+class ZomatoQ {
+
+public static double search(String foodName){
+ double price = 0.0;
+	 if(foodName == "Garlic bread"){
+	  price = 110;
+	  return price;
+	   }
+	 
+	 if(foodName == "Veg burger"){
+	   price = 85;
+	  return price;
+	   }
+	 
+	 if(foodName == "Idly"){
+	   price = 50;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Masala dosa"){
+	   price = 70;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Halwa"){
+	   price = 45;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Parota"){
+	   price = 35;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Vanilla icecream"){
+	   price = 100;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "DBC"){
+	   price = 210;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Akki rotti"){
+	   price = 40;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Lemon rice"){
+	   price = 45;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Sandwich"){
+	   price = 90;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Rolls"){
+	   price = 70;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Milkshake"){
+	   price = 65;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mango juice"){
+	   price = 55;
+	  return price;
+	   }
+	
+	 if(foodName == "Samosa"){
+	   price = 25;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chocolate cake"){
+	   price = 350;
+	  return price;
+	   }
+	   
+	 if(foodName == "Tomato soup"){
+	   price = 95;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fried momos"){
+	   price = 120;
+	  return price;
+	   }
+	   
+	 if(foodName == "Gobi manchurian"){
+	   price = 80;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pav bhaji"){
+	   price = 75;
+	  return price;
+	   }
+	 
+	 if(foodName == "Paneer butter masala"){
+	   price = 175;
+	  return price;
+	   }
+	 
+	 if(foodName == "Baby corn manchurian"){
+	   price = 115;
+	  return price;
+	   }
+	   
+	 if(foodName == "Aloo parata"){
+	   price = 35;
+	  return price;
+	   }
+	   
+	 if(foodName == "Veg thali"){
+	   price = 150;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pav bhaji"){
+	   price = 75;
+	  return price;
+	   }
+	   
+	 if(foodName == "Rabdi"){
+	   price = 85;
+	  return price;
+	   }
+	   
+	 if(foodName == "Rasagulla"){
+	   price = 70;
+	  return price;
+	   }
+	   
+	 if(foodName == "Dharwad peda"){
+	   price = 150;
+	  return price;
+	   }
+	   
+	 if(foodName == "Puliyogare"){
+	   price = 80;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pasta"){
+	   price = 90;
+	  return price;
+	   }
+	      
+	 if(foodName == "Fruit salad"){
+	   price = 120;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mushroom curry"){
+	   price = 165;
+	  return price;
+	   }
+	   
+	 if(foodName == "Veg sushi"){
+	   price = 155;
+	  return price;
+	   }
+	   
+	 if(foodName == "Veg rolls"){
+	   price = 45;
+	  return price;
+	   }
+	   
+	 if(foodName == "Vegetable salad"){
+	   price = 90;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fried rice"){
+	   price = 75;
+	  return price;
+	   }
+	   
+	 if(foodName == "North indian thali"){
+	   price = 185;
+	  return price;
+	   }
+	   
+	 if(foodName == "Noodles"){
+	   price = 100;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chole bhature"){
+	   price = 80;
+	  return price;
+	   }
+	   
+	 if(foodName == "Andhra meals"){
+	   price = 115;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mushroom curry"){
+	   price = 165;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pastry"){
+	   price = 85;
+	  return price;
+	   }
+	   
+	 if(foodName == "Strawberry shake"){
+	   price = 120;
+	  return price;
+	   }
+	   
+	 if(foodName == "Khichdi"){
+	   price = 80;
+	  return price;
+	   }
+	   
+	 if(foodName == "Dal"){
+	   price = 90;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fries"){
+	   price = 110;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kachori"){
+	   price = 40;
+	  return price;
+	   }
+	   
+	 if(foodName == "Curd rice"){
+	   price = 80;
+	  return price;
+	   }
+	   
+	 if(foodName == "Cold coffee"){
+	   price = 120;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pulao"){
+	   price = 75;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kulche"){
+	   price = 50;
+	  return price;
+	   }
+	    
+
+	return price;
+}
+
+
+
+
+public static double search(String foodName, int quantity){
+ double price = 0.0;
+	 if(foodName == "Garlic bread"){
+	  price = 110*quantity;
+	  return price;
+	   }
+	 
+	 if(foodName == "Veg burger"){
+	   price = 85*quantity;
+	  return price;
+	   }
+	 
+	 if(foodName == "Idly"){
+	   price = 50*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Masala dosa"){
+	   price = 70*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Halwa"){
+	   price = 45*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Parota"){
+	   price = 35*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Vanilla icecream"){
+	   price = 100*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "DBC"){
+	   price = 210*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Akki rotti"){
+	   price = 40*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Lemon rice"){
+	   price = 45*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Sandwich"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Rolls"){
+	   price = 70*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Milkshake"){
+	   price = 65*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mango juice"){
+	   price = 55*quantity;
+	  return price;
+	   }
+	
+	 if(foodName == "Samosa"){
+	   price = 25*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chocolate cake"){
+	   price = 350*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Tomato soup"){
+	   price = 95*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fried momos"){
+	   price = 120*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Gobi manchurian"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pav bhaji"){
+	   price = 75*quantity;
+	  return price;
+	   }
+	 
+	 if(foodName == "Paneer butter masala"){
+	   price = 175*quantity;
+	  return price;
+	   }
+	 
+	 if(foodName == "Baby corn manchurian"){
+	   price = 115*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Aloo parata"){
+	   price = 35*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Veg thali"){
+	   price = 150*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pav bhaji"){
+	   price = 75*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Rabdi"){
+	   price = 85*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Rasagulla"){
+	   price = 70*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Dharwad peda"){
+	   price = 150*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Puliyogare"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pasta"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	      
+	 if(foodName == "Fruit salad"){
+	   price = 120*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mushroom curry"){
+	   price = 165*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Veg sushi"){
+	   price = 155*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Veg rolls"){
+	   price = 45*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Vegetable salad"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fried rice"){
+	   price = 75*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "North indian thali"){
+	   price = 185*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Noodles"){
+	   price = 100*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chole bhature"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Andhra meals"){
+	   price = 115*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mushroom curry"){
+	   price = 165*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pastry"){
+	   price = 85*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Strawberry shake"){
+	   price = 120*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Khichdi"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Dal"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fries"){
+	   price = 110*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kachori"){
+	   price = 40*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Curd rice"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Cold coffee"){
+	   price = 120*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Pulao"){
+	   price = 75*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kulche"){
+	   price = 50*quantity;
+	  return price;
+	   }
+	    
+
+	return price;
+}
+}
