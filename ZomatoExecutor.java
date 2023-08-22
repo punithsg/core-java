3188ebfd8e846cb4741d39ef2e533dd01ac12d85
Prashantb07 <prashantb.xworkz@gmail.com>
2023-08-22 13:53:18 +0530

    method over loading

---
+++ ZomatoExecutor.java
@@ -0,0 +1,11 @@
+class ZomatoExecutor {
+
+	public static void main(String foodDelivery[]){
+	System.out.println("main started");
+	
+	Zomato.search("Garlic bread");
+	
+	System.out.println("main ended");
+	
+	}
+}
