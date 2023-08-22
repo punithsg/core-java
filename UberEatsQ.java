8fc9359b1eb3954d86bf25b4faeff121f52209d5
Prashantb07 <prashantb.xworkz@gmail.com>
2023-08-22 11:36:04 +0530

    method overloading example

---
+++ UberEatsQ.java
@@ -0,0 +1,426 @@
+class UberEatsQ {
+
+ public static double search (String foodName){
+	double price = 0.0;
+  	 
+	 if(foodName == "Guntur chicken"){
+	   price = 210;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Hyderabadi biriyani"){
+	   price = 40;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Mutton boti fry"){
+	   price = 45;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Chicken pepper dry"){
+	   price = 90;
+	  return price;
+	   }
+	   
+	   if(foodName == "Parota"){
+	   price = 35;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Vanilla icecream"){
+	   price = 100;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "DBC"){
+	   price = 210;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Akki rotti"){
+	   price = 40;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Lemon rice"){
+	   price = 45;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Sandwich"){
+	   price = 90;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Rolls"){
+	   price = 70;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Milkshake"){
+	   price = 65;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Grill chicken"){
+	   price = 70;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Kebab"){
+	   price = 65;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chicken lollipop"){
+	   price = 55;
+	  return price;
+	   }
+	
+	 if(foodName == "Chicken tandoori"){
+	   price = 25;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kalmi kebab"){
+	   price = 350;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fish fry"){
+	   price = 95;
+	  return price;
+	   }
+	   
+	 if(foodName == "chicken momos"){
+	   price = 120;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kheema parata"){
+	   price = 80;
+	  return price;
+	   }
+	   
+	 if(foodName == "Liver fry"){
+	   price = 75;
+	  return price;
+	   }
+	 
+	 if(foodName == "Muttuon kadai"){
+	   price = 175;
+	  return price;
+	   }
+	 
+	 if(foodName == "Boneless chicken"){
+	   price = 115;
+	  return price;
+	   }
+	   
+	 if(foodName == "Prawn curry"){
+	   price = 35;
+	  return price;
+	  
+	 if(foodName == "Sheer kurma"){
+	   price = 85;
+	  return price;
+	   }
+	   
+	 if(foodName == "Donne mutton biriyani"){
+	   price = 280;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mirchi bajji"){
+	   price = 60;
+	  return price;
+	   }
+	   
+	 if(foodName == "Rasamalai"){
+	   price = 90;
+	  return price;
+	   }
+	   
+	 if(foodName == "Uttapam"){
+	   price = 70;
+	  return price;
+	   }
+	   
+	   
+	 if(foodName == "Kulfi"){
+	   price = 60;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Dry fruit sundae"){
+	   price = 210;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Corn dosa"){
+	   price = 80;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Pista shake"){
+	   price = 85;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Kashmiri falooda"){
+	   price = 190;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Peri peri Fries"){
+	   price = 90;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Mcspicy chicken"){
+	   price = 165;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chicken kebab burger"){
+	   price = 155;
+	  return price;
+	   }
+	    
+
+	return price;
+}
+
+
+
+
+
+ public static double search(String foodName, int quantity){
+	double price = 0.0;
+  	 
+	 if(foodName == "Guntur chicken"){
+	   price = 210*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Hyderabadi biriyani"){
+	   price = 40*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Mutton boti fry"){
+	   price = 45*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Chicken pepper dry"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	   
+	   if(foodName == "Parota"){
+	   price = 35*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Vanilla icecream"){
+	   price = 100*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "DBC"){
+	   price = 210*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Akki rotti"){
+	   price = 40*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Lemon rice"){
+	   price = 45*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Sandwich"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Rolls"){
+	   price = 70*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Milkshake"){
+	   price = 65*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Grill chicken"){
+	   price = 70*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Kebab"){
+	   price = 65*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chicken lollipop"){
+	   price = 55*quantity;
+	  return price;
+	   }
+	
+	 if(foodName == "Chicken tandoori"){
+	   price = 25*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kalmi kebab"){
+	   price = 350*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Fish fry"){
+	   price = 95*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "chicken momos"){
+	   price = 120*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Kheema parata"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Liver fry"){
+	   price = 75*quantity;
+	  return price;
+	   }
+	 
+	 if(foodName == "Muttuon kadai"){
+	   price = 175*quantity;
+	  return price;
+	   }
+	 
+	 if(foodName == "Boneless chicken"){
+	   price = 115*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Prawn curry"){
+	   price = 35*quantity;
+	  return price;
+	  
+	 if(foodName == "Sheer kurma"){
+	   price = 85*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Donne mutton biriyani"){
+	   price = 280*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Mirchi bajji"){
+	   price = 60*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Rasamalai"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Uttapam"){
+	   price = 70*quantity;
+	  return price;
+	   }
+	   
+	   
+	 if(foodName == "Kulfi"){
+	   price = 60*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Dry fruit sundae"){
+	   price = 210*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Corn dosa"){
+	   price = 80*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Pista shake"){
+	   price = 85*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Kashmiri falooda"){
+	   price = 190*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Peri peri Fries"){
+	   price = 90*quantity;
+	  return price;
+	   }
+	
+	 
+	 if(foodName == "Mcspicy chicken"){
+	   price = 165*quantity;
+	  return price;
+	   }
+	   
+	 if(foodName == "Chicken kebab burger"){
+	   price = 155*quantity;
+	  return price;
+	   }
+	    
+
+	return price;
+}
+}
