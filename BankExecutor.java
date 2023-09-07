296391ae03157ebef1d0ebe3cd5000d7690911dd
punithsg <142565990+punithsg@users.noreply.github.com>
2023-09-07 10:28:40 +0530

    Update BankExecutor.java

--- BankExecutor.java
+++ BankExecutor.java
@@ -1,24 +1,22 @@
 class BankExecutor{
-public static void main(String args[]){
-
-   Bank bank = new Bank();
-   bank.setBankId(3);
-   bank.setBankName("Bank of Bharath");
-   bank.setIfsc(56789);
-   bank.setMic(54365565);
-   bank.setBranchNo(67);
-   bank.setLocation("Vijipura");
-   bank.setManagerName("Kalinga");
-   bank.setNoOfemployees(78);
-   
-   System.out.println("the bank id is :" +bank.getBankId());
-   System.out.println("the bank name is :" +bank.getBankName());
-   System.out.println("the bank ifsc code is :" +bank.getIfsc());
-   System.out.println("the bank mic is :" +bank.getMic());
-   System.out.println("the bank branch no is :" +bank.getBranchNo());
-   System.out.println("the bank location is :" +bank.getLocation());
-   System.out.println("the manager name is :" +bank.getManagerName());
-   System.out.println("the employees are :" +bank.getNoOfemployees());
-   
-}
+	
+	public static void main(String args[]){
+		
+		Bank bank = new Bank();
+		
+		bank.setBankId(1);
+		bank.setName("Karnataka Bank");
+		bank.setBranchName("S S puram");
+		bank.setManagerName("Ramesh");
+		bank.setIfscCode("KARB0000757");
+		bank.setEmail("karnatakabank@gmail.in");
+		
+		System.out.println("The Bank Id Is:  " + bank.getBankId());
+		System.out.println("The Bank Name Is:  " + bank.getName());
+		System.out.println("Branch Name Is:  "  + bank.getBranchName());
+		System.out.println("Manager Name Is:  " + bank.getManagerName());
+		System.out.println("IFSC Code Is:  " + bank.getIfscCode());
+		System.out.println("The Bank Email Is: " + bank.getEmail());
+	}
+	
 }
